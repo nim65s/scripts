63c40ab84729144845c4a31380e68b9ad82a24be
Guilhem Saurel <nim65s.garbage@gmail.com>
2010-05-02 21:30:03 +0200

    nettoyage pour publication

--- garfield.sh
+++ garfield.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-#TODO : merge du de la seconde boucle avec son test interne
-
 export DISPLAY=:0.1
 cd $HOME/images
 
 if [[ -e ga$(date '+%y%m%d' --date '1 days ago').gif ]]
-    then
-	echo "image déjà vue"
-    else
-	wget "http://picayune.uclick.com/comics/ga/$(date '+%Y' --date '1 days ago')/ga$(date '+%y%m%d' --date '1 days ago').gif"
-	feh -ZF ga$(date '+%y%m%d' --date '1 days ago').gif
-    fi
-for FILE in $(echo ga*.gif | grep -v ga\*.gif)
-    do
-	if [[ "$FILE" != "ga$(date '+%y%m%d' --date '1 days ago').gif" ]]
-	    then
-		rm $FILE
-	    fi
-    done
+  then
+    echo "image déjà vue"
+  else
+    wget "http://picayune.uclick.com/comics/ga/$(date '+%Y' --date '1 days ago')/ga$(date '+%y%m%d' --date '1 days ago').gif"
+    feh -ZF ga$(date '+%y%m%d' --date '1 days ago').gif
+  fi
+for FILE in $(echo ga*.gif | grep -v 'ga\*.gif' | grep -v ga$(date '+%y%m%d' --date '1 days ago').gif)
+  do
+    rm $FILE
+  done
 exit 0

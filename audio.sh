47461e518957639a0174945655517917653f1ec1
Nim65s <nim65s@gmail.com>
2010-12-24 11:56:46 +0100

    commit de Nowël ! :D

--- audio.sh
+++ audio.sh
@@ -5,7 +5,18 @@ server=oss
 
 add_random()
 {
-		mpc listall | sed -n $((RANDOM % $(mpc stats | grep Songs | awk '{print $2}')+1))p | mpc add
+		if [[ -n $1 ]]
+		then
+				if [[ -f $1 ]]
+				then
+						playlist="$1"
+				else
+						playlist=/var/lib/mpd/playlists/$1.m3u
+				fi
+				cat $playlist | sed 's/[/]mnt[/]T[/]musik[/]//' | sed -n $((RANDOM % $(wc -l $1 | cut -d' ' -f 1)+1))p | mpc add
+		else
+				mpc listall | sed -n $((RANDOM % $(mpc stats | grep Songs | awk '{print $2}')+1))p | mpc add
+		fi
 }
 
 case $1 in
@@ -58,7 +69,7 @@ case $1 in
 								mpc del 0
 							#	if [[ "$(mpc playlist | wc -l)" == "2" ]]
 						#		then
-						#				add_random
+						#				add_random $2
 						#		fi
 						else
 								mpc next
@@ -70,24 +81,25 @@ case $1 in
 				if [[ "$player" == "mpd" ]]
 				then
 						mpc del 2
-						add_random
+						add_random $2
 				fi
 				;;
 		n3) # change the the song after the next
 				if [[ "$player" == "mpd" ]]
 				then
 						mpc del 3
-						add_random
+						add_random $2
 				fi
 				;;
 		r) # enter in Nim's Random mode
 				if [[ "$player" == "mpd" ]]
 				then
+						add_random $2
+						add_random $2
+						echo "mpdmode.text ='C'" | awesome-client
+						mpc play
 						mpc consume on
 						mpc crop
-						add_random
-						add_random
-						echo "mpdmode.text ='C'" | awesome-client
 				fi
 
 				;;
@@ -100,7 +112,7 @@ case $1 in
 		ar) # add a random song from the database
 				if [[ "$player" == "mpd" ]]
 				then
-						add_random
+						add_random $2
 				fi
 esac
 

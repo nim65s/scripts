ae35017e87a476fc1c38eec38c0d237b8f78f8d2
nim65s.garbage@gmail.com <nim65s.garbage@gmail.com>
2011-05-10 13:56:40 +0200

    autototoro

--- majgit.sh
+++ majgit.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+
 cd ~/dotfiles
+echo -e "\t\t == pull dotfiles =="
 git pull origin master
 git commit -a
+echo -e "\t\t == push dotfiles =="
 git push origin master
 
 cd ~/scripts
+echo -e "\t\t == pull scripts =="
 git pull origin master
 git commit -a
+echo -e "\t\t == push scripts =="
 git push origin master
+
+if [[ "$(hostname)" != "totoro" ]]
+then
+    echo -e "\t\t === goto totoro ==="
+    ssh nim@nim65s.dyndns.org majgit
+fi

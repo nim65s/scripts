c4c80e30ee54bb3aac5cff0943616cd15028b558
nim65s.garbage@gmail.com <nim65s.garbage@gmail.com>
2011-06-28 16:52:30 +0200

    option pour generateTexMakefile : triple compilation que si nécessaire.

--- generateTexMakefile.sh
+++ generateTexMakefile.sh
@@ -3,31 +3,33 @@
 [[ -f Makefile ]] && rm -v Makefile
 [[ -f Makefile.quick ]] && rm -v Makefile.quick
 
-[[ $1 == "-x" ]] && TEX="xelatex" || TEX="pdflatex"
+[[ $1 == "*x*" ]] && TEX="xelatex" || TEX="pdflatex"
 
 
 echo -n "all: " >> Makefile
 for FILE in *.tex
-do
-        echo -n "$(echo "$FILE " | grep -v '*' | sed 's/tex/pdf/')" >> Makefile
+do echo -n "$(echo "$FILE " | grep -v '*' | sed 's/tex/pdf/')" >> Makefile
 done
 echo clean >> Makefile
 echo >> Makefile
 
 echo '%.pdf: %.tex' >> Makefile
 echo -en "\t" >> Makefile
-echo "( ( $TEX -shell-escape $< || ( rm \$@ && false ) ) && $TEX -shell-escape $< || ( rm \$@ && false ) ) && $TEX -shell-escape $<" >> Makefile
+if [[ $1 != "*m*" ]] 
+then echo "$TEX -shell-escape $<" >> Makefile
+else echo "(( $TEX -shell-escape $< || ( rm \$@ && false )) && $TEX -shell-escape $< || ( rm \$@ && false )) && $TEX -shell-escape $<" >> Makefile
+fi
 echo >> Makefile
 echo 'clean:' >> Makefile
 echo -en "\t" >> Makefile
 echo '-rm -vf *.aux *.log *.nav *.out *.snm *.toc *.tmp *~ 2> /dev/null' >> Makefile
 
-
-sed "s/( ( $TEX -shell-escape $< || ( rm \$@ && false ) ) && $TEX -shell-escape $< || ( rm \$@ && false ) ) && $TEX -shell-escape $</$TEX -shell-escape $</" Makefile > Makefile.quick
-
-chmod +x Makefile Makefile.quick
+[[ $1 == "*m*" ]] && sed "s/(( $TEX -shell-escape $< || ( rm \$@ && false )) && $TEX -shell-escape $< || ( rm \$@ && false )) && $TEX -shell-escape $</$TEX -shell-escape $</" Makefile > Makefile.quick
 
 echo "---------Makefile---------"
 cat Makefile
-echo "------Makefile.quick------"
-cat Makefile.quick
+if [[ $1 == "*m*" ]]
+then
+    echo "------Makefile.quick------"
+    cat Makefile.quick
+fi

3feb8e8f199ca5c2b31172bb5be3fe1b7ca3cbcb
nim65s.garbage@gmail.com <nim65s.garbage@gmail.com>
2011-06-30 16:17:51 +0200

    trop de quotes

--- generateTexMakefile.sh
+++ generateTexMakefile.sh
@@ -3,7 +3,7 @@
 [[ -f Makefile ]] && rm -v Makefile
 [[ -f Makefile.quick ]] && rm -v Makefile.quick
 
-[[ $1 == "*x*" ]] && TEX="xelatex" || TEX="pdflatex"
+[[ $1 == *x* ]] && TEX="xelatex" || TEX="pdflatex"
 
 
 echo -n "all: " >> Makefile
@@ -15,7 +15,7 @@ echo >> Makefile
 
 echo '%.pdf: %.tex' >> Makefile
 echo -en "\t" >> Makefile
-if [[ $1 != "*m*" ]] 
+if [[ $1 != *m* ]] 
 then echo "$TEX -shell-escape $<" >> Makefile
 else echo "(( $TEX -shell-escape $< || ( rm \$@ && false )) && $TEX -shell-escape $< || ( rm \$@ && false )) && $TEX -shell-escape $<" >> Makefile
 fi
@@ -24,11 +24,11 @@ echo 'clean:' >> Makefile
 echo -en "\t" >> Makefile
 echo '-rm -vf *.aux *.log *.nav *.out *.snm *.toc *.tmp *~ 2> /dev/null' >> Makefile
 
-[[ $1 == "*m*" ]] && sed "s/(( $TEX -shell-escape $< || ( rm \$@ && false )) && $TEX -shell-escape $< || ( rm \$@ && false )) && $TEX -shell-escape $</$TEX -shell-escape $</" Makefile > Makefile.quick
+[[ $1 == *m* ]] && sed "s/(( $TEX -shell-escape $< || ( rm \$@ && false )) && $TEX -shell-escape $< || ( rm \$@ && false )) && $TEX -shell-escape $</$TEX -shell-escape $</" Makefile > Makefile.quick
 
 echo "---------Makefile---------"
 cat Makefile
-if [[ $1 == "*m*" ]]
+if [[ $1 == *m* ]]
 then
     echo "------Makefile.quick------"
     cat Makefile.quick

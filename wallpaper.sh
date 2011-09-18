9d2ec1d6bcfb365ef1174231fd02198b201e4ea9
Guilhem Saurel <nim@Xlaptop.(none)>
2011-09-18 13:19:40 +0200

    wallpaper=f(hostname)

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-export DISPLAY=:0.0
-
-
-awsetbg -r /home/nim/images/wallpaper/ -u feh
+if [[ "$1" == "hostname" ]]
+then
+    if [[ -e "$HOME/images/hostname/`hostname`.jpg"]]
+    then
+        awsetbg "$HOME/images/hostname/`hostname`.jpg"
+    else
+        awsetbg "$HOME/images/hostname/totoro_parapluie.jpg"
+    fi
+else
+    export DISPLAY=:0.0
+    awsetbg -r /home/nim/images/wallpaper/ -u feh
+fi
 
 exit

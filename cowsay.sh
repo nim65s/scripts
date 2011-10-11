82a6b11143a4e5505911d34ca2b8abacea456ecd
nim65s.garbage@gmail.com <nim65s.garbage@gmail.com>
2011-10-11 16:57:22 +0200

    mass xcowsay : sacha & pikachu

---
+++ cowsay.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+case $1 in 
+    pikachu)
+        CMD="--image /home/saurelg/images/xcowsay/pikachu.png PIKACHU !"
+        ;;
+    sacha)
+        CMD="--image /home/saurelg/images/xcowsay/sacha.png GOTTA CATCH THEM ALL !"
+        ;;
+    *)
+        $1
+        ;;
+esac
+
+
+for client in amy bender fry leela hermes farnsworth scruffy
+do
+    for i in 1 2 3 4 5 
+    do
+        ssh $client DISPLAY=:$i xcowsay $CMD &
+    done
+done

292c7c163a8bb78a579b4361969b4c32e75883ec
Guilhem Saurel <guilhem.saurel@gmail.com>
2012-01-12 23:47:09 +0100

    nouveau script de gestion des VCS

---
+++ push.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+git=(N7 dotfiles scripts AOC_LaTeX div/vimpager)
+hg=(net7/admin net7/botnet7-ng net7/doc net7/docs net7/pipo-parici net7/portail net7/scripts_live)
+
+for d in ${git[*]} 
+do
+    cd ~/$d
+    echo -e "\t\t\033[1;32m git : $d \033[m"
+    pwd
+    git pull
+    git commit -a
+    git push
+done
+
+#for d in ${hg[*]}
+#do
+#    cd ~/$d
+#    echo -e "\t\t033[1;32m hg : $d 033[m"
+#    pwd
+#    hg pull
+#    hg commit
+#    hg push
+#done

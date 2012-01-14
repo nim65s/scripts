3af840377402b01a27034143fa96d0c37e018efa
Guilhem Saurel <guilhem.saurel@gmail.com>
2012-01-14 12:28:03 +0100

    je me connais; je vais pas déplacer le dossier vimpager tout de suite…

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git=(N7 dotfiles scripts AOC_LaTeX div/vimpager)
+git=(N7 dotfiles scripts AOC_LaTeX vimpager)
 hg=(net7/admin net7/botnet7-ng net7/doc net7/docs net7/pipo-parici net7/portail net7/scripts_live)
 
 for d in ${git[*]} 

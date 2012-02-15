8d672f0efa3283f3ffeca12faa11efdca5bed509
Guilhem Saurel <guilhem.saurel@gmail.com>
2012-02-15 21:05:15 +0100

    faut pas mettre de CMakeLists

--- newCproject.sh
+++ newCproject.sh
@@ -14,22 +14,22 @@ echo -e "\t\t on va dans le répertoire $nom"
 cd $nom
 mkdir -v bin build src
 echo -e "Création du CMakeLists.txt..."
-echo -e "cmake_minimum_required(VERSION 2.6)" > build/CMakeLists.txt
-echo -e "project($nom)" >> build/CMakeLists.txt
-echo -e "" >> build/CMakeLists.txt
-echo -e "file(" >> build/CMakeLists.txt
-echo -e "\tGLOB_RECURSE" >> build/CMakeLists.txt
-echo -e "\tsource_files" >> build/CMakeLists.txt
-echo -e "\t../src/*" >> build/CMakeLists.txt
-echo -e "\t)" >> build/CMakeLists.txt
-echo -e "" >> build/CMakeLists.txt
-echo -e "add_executable(" >> build/CMakeLists.txt
-echo -e "\t../bin/$nom" >> build/CMakeLists.txt
-echo -e "\t\${source_files}" >> build/CMakeLists.txt
-echo -e "\t)" >> build/CMakeLists.txt
-echo -e 'set(CMAKE_CXX_FLAGS "-g -Wall -pedantic -Wextra")' >> build/CMakeLists.txt
+echo -e "cmake_minimum_required(VERSION 2.6)" > CMakeLists.txt
+echo -e "project($nom)" >> CMakeLists.txt
+echo -e "" >> CMakeLists.txt
+echo -e "file(" >> CMakeLists.txt
+echo -e "\tGLOB_RECURSE" >> CMakeLists.txt
+echo -e "\tsource_files" >> CMakeLists.txt
+echo -e "\t../src/*" >> CMakeLists.txt
+echo -e "\t)" >> CMakeLists.txt
+echo -e "" >> CMakeLists.txt
+echo -e "add_executable(" >> CMakeLists.txt
+echo -e "\t../bin/$nom" >> CMakeLists.txt
+echo -e "\t\${source_files}" >> CMakeLists.txt
+echo -e "\t)" >> CMakeLists.txt
+echo -e 'set(CMAKE_CXX_FLAGS "-g -Wall -pedantic -Wextra")' >> CMakeLists.txt
 touch src/main.cpp
 cd build
-cmake . -G "Unix Makefiles"
+cmake .. -G "Unix Makefiles"
 
 exit 0

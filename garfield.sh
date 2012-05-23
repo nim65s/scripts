7cd6eaf2ec058c22619e97aab1e00fd8ed2e8bc0
Guilhem Saurel <guilhem.saurel@gmail.com>
2012-05-23 21:58:13 +0200

    réduction drastique de la verbosité superflue de garfield

--- garfield.sh
+++ garfield.sh
@@ -3,7 +3,7 @@
 cd $HOME/images
 
 [[ -e garfield.gif ]] && rm garfield.gif
-wget -nv "http://picayune.uclick.com/comics/ga/$(date '+%Y' --date 'yesterday')/ga$(date '+%y%m%d' --date 'yesterday').gif" -O garfield.gif
+wget -nv "http://picayune.uclick.com/comics/ga/$(date '+%Y' --date 'yesterday')/ga$(date '+%y%m%d' --date 'yesterday').gif" -O garfield.gif > /dev/null
 feh -ZF garfield.gif
 
 exit 0

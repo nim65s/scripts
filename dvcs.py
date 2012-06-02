075a74658230bdc6a1cc57878c2e1bfb4baa206f
Guilhem Saurel <guilhem.saurel@gmail.com>
2012-06-02 13:45:03 +0200

    dvcs.py debuggé

--- dvcs.py
+++ dvcs.py
@@ -3,6 +3,8 @@
 
 #TODO: fenetres de textes dans les fenetres, pour pas faire clignoter bordures & titres
 
+e = ''
+
 import locale, curses, time, logging
 from os import chdir
 from sys import argv
@@ -23,14 +25,15 @@ log.addHandler(logging.FileHandler(logfile))
 YW = 40 # Largeur type d’une fenêtre
 YH = 28 # Hauteur
 #N = 13 # Nombre de fenetres
-N = 2 # Nombre de fenetres
+N = 3 # Nombre de fenetres
 
-class NimWindow():
-    def __init__(self, height, width, j, k, nbr_win_y, reste_y, dvcs, path):
+class NimWindow(Thread):
+    def __init__(self, height, width, j, k, nbr_win_y, reste_y, dvcs, path, stdscr, max_y):
+        Thread.__init__(self)
         self.path = path
         self.dvcs = dvcs
         # Fenêtre: taille, bordure
-        self.ncols = width # Colonnes utiles
+        self.ncols = width - 1 # Colonnes utiles (-1 pour la bordure droite)
         self.nlines = height - 2 # Lignes utiles (-1 pour le titre et -1 pour la bordure basse)
         begin_y = j*width
         rs = curses.ACS_VLINE
@@ -40,28 +43,31 @@ class NimWindow():
             rs = ' '
             tr = ' '
             br = curses.ACS_HLINE
+            self.ncols += 1
         if j < reste_y:
             width += 1
+            self.ncols += 1
         begin_y += min(j,reste_y)
         self.win = curses.newwin(height,width,k*height,begin_y)
-        self.win.border(' ',rs,' ',curses.ACS_HLINE,' ',tr,curses.ACS_HLINE,br)
+        self.win.border(' ',rs,' ',curses.ACS_HLINE,' ',tr,curses.ACS_HLINE,'X')
 
         # Fenêtre: titre
         if len(dvcs)+len(path) < self.ncols + 1:
             spaces = self.ncols - len(dvcs) - len(path) - 2
-            self.title = ' ' * (spaces/2) + dvcs + ': ' + path + ' ' * (spaces/2 - spaces%2)
+            self.title = ' ' * (spaces/2) + dvcs + ': ' + path + ' ' * (spaces/2 - 2)
         else:
             title = dvcs + ': ' + path
             self.title = title[:self.ncols]
         self.win.addstr(0,1,self.title)
+        #self.win.addstr(0,1, '%s %s %s %s %s' % (self.ncols,self.nlines,width,begin_y, max_y))
 
         # Fenêtre: lignes utiles
         self.lines = []
         for i in range(self.nlines):
             self.lines.append(' ')
-        self.refresh()
+        #self.refresh()
 
-    def __call__(self):
+    def run(self):
         chdir(expanduser(self.path))
         p = Popen(['cat','st'],stdout=PIPE)
         while 1:
@@ -74,6 +80,7 @@ class NimWindow():
     def addline(self, line):
         lines = []
         #log.info('addline %s' % line)
+        line = line.replace('\t','    ')
         while len(line) > self.ncols:
             lines.append(line[:self.ncols])
             #log.info('→' + line[:self.ncols] + '−' + line[self.ncols:])
@@ -93,68 +100,50 @@ class NimWindow():
 
     def refresh(self):
         for i in range(self.nlines):
-            self.win.addstr(self.nlines - i,0,self.lines[i])
+            line = self.lines[i] + ' ' * (self.ncols-len(self.lines[i]))
+            self.win.addstr(self.nlines - i,0,line)
             self.win.refresh()
-            time.sleep(1)
-        #self.win.clear()#TODO refresh/clear
-        self.win.refresh()#TODO refresh/clear
+            time.sleep(0.001)
+        self.win.refresh()
 
 stdscr = curses.initscr()
 curses.noecho()
 curses.cbreak()
 stdscr.keypad(1)
 stdscr.refresh()
+try:
+    max_x,max_y = stdscr.getmaxyx()
+    if YW > max_y:
+        YW = max_y
 
-max_x,max_y = stdscr.getmaxyx()
+    nbr_win_y = max_y / YW # Nombre de fenetres sur une ligne
+    wid_win_y = YW + ( max_y % YW ) / ( nbr_win_y ) # leur largeur
+    if nbr_win_y >= N:
+        nbr_win_y = N
+        wid_win_y = max_y / N #- N + 1
+    reste_y = max_y - nbr_win_y * wid_win_y #+1 # ce qu’il reste
 
-nbr_win_y = max_y / YW # Nombre de fenetres sur une ligne
-if nbr_win_y > N:
-    nbr_win_y = N
-wid_win_y = YW + ( max_y % YW ) / ( nbr_win_y ) # leur largeur
-reste_y = max_y - nbr_win_y * wid_win_y #+1 # ce qu’il reste
-
-i = 0
-j = 0
-k = 0
-while i < N:
-    while j < nbr_win_y and i < N:
-        Thread(target = NimWindow(YH, wid_win_y, j, k, nbr_win_y, reste_y, 'git', '~/AOC_LaTeX')).start()
-        j += 1
-        i += 1
+    i = 0
     j = 0
-    k += 1
+    k = 0
+    while i < N:
+        while j < nbr_win_y and i < N:
+            t = NimWindow(YH, wid_win_y, j, k, nbr_win_y, reste_y, 'git', '~/AOC_LaTeX',stdscr, max_y)
+            t.run()
+            j += 1
+            i += 1
+        j = 0
+        k += 1
 
+except NameError as er:
+    print er
+    e = er
 
-time.sleep(6)
-#stdscr.addstr('bla')
-#stdscr.refresh()
-#time.sleep(1)
-#stdscr.addstr(stdscr.getmaxyx().__str__())
-#stdscr.refresh()
-#time.sleep(1)
+finally:
+    curses.nocbreak()
+    stdscr.keypad(0)
+    curses.echo()
+    curses.endwin()
 
-#begin_x = 20
-#begin_y = 7
-#height = 5
-#width = 40
-#win = curses.newwin(height, width, begin_y, begin_x)
-#win.box()
-#win.refresh()
-#time.sleep(1)
-#win.addstr('blabla')
-#win.refresh()
-#time.sleep(1)
-#win.addstr(win.getmaxyx().__str__())
-#win.refresh()
-#time.sleep(1)
-#win.addstr('\n1234567890123456789012345678901234567890')
-#win.refresh()
-#time.sleep(3)
-
-curses.nocbreak()
-stdscr.keypad(0)
-curses.echo()
-curses.endwin()
-
-print 'pipo'
+print 'pipo', e
 #time.sleep(1)

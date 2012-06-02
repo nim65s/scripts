7a350329897b4adebb3a9571dad45fa760c424f7
Guilhem Saurel <guilhem.saurel@gmail.com>
2012-06-02 04:21:36 +0200

    ajout des Hg à git.sh \o/

--- git.sh
+++ git.sh
@@ -5,9 +5,9 @@ PULL=false
 COMMIT=false
 PUSH=false
 
-git=(N7 dotfiles scripts CV JE gdf)
+git=(N7 dotfiles scripts CV JE gdf net7/bots/pipobot)
 [[ $(hostname) == "totoro" ]] && git=(${git[*]} AOC_LaTeX)
-hg=(net7/admin net7/botnet7-ng net7/doc net7/docs net7/pipo-parici net7/portail net7/scripts_live)
+hg=(net7/admin net7/bots/pipobot-modules net7/doc net7/docs net7/portail net7/scripts_live)
 
 if [[ "$1" == "status" ]]
 then
@@ -45,13 +45,15 @@ do
     fi
 done
 
-# TODO: auth HG
-#for d in ${hg[*]}
-#do
-#    cd ~/$d
-#    echo -e "\t\t033[1;32m hg : $d 033[m"
-#    pwd
-#    hg pull
-#    hg commit
-#    hg push
-#done
+for d in ${hg[*]}
+do
+    if [[ -d ~/$d ]]
+    then
+        cd ~/$d
+        echo -e "\t\t\033[1;32m hg : $d \033[m"
+        $STATUS && hg st
+        $PULL && hg pull -u
+        $COMMIT && hg ci
+        $PUSH && hg push
+    fi
+done

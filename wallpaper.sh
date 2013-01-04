21b2db184facfbe4a2c0a352d019b79275d9773f
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-01-04 16:02:15 +0100

    wallpaper: passage d’awsetbg à nitrogen

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-if [[ "$1" == "hostname" ]]
-then
-    cd $HOME/images/hostname || exit 1
-    if [[ -e `hostname`.jpg ]]
-    then awsetbg `hostname`.jpg
-    elif [[ -e `hostname`.png ]]
-    then awsetbg `hostname`.png
-    else awsetbg ghibli.png
-    fi
-else
-    awsetbg -r $HOME/images/wallpaper -u feh
-fi
+[[ -x /usr/local/bin/nitrogen ]] || echo "need git://github.com/l3ib/nitrogen.git" && exit 1
 
-exit
+cd ~/images/wallpaper
+/usr/local/bin/nitrogen --head=0 --set-zoom-fill $(ls|shuf -n1)

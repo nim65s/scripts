cbddfa4c89562dda8db3ac0bcb7210a3b1ee242b
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-01-04 16:08:19 +0100

    wallpaper: forgottent ()

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ -x /usr/local/bin/nitrogen ]] || echo "need git://github.com/l3ib/nitrogen.git" && exit 1
+[[ -x /usr/local/bin/nitrogen ]] || (echo "need git://github.com/l3ib/nitrogen.git" && exit 1)
 
 cd ~/images/wallpaper
 /usr/local/bin/nitrogen --head=0 --set-zoom-fill $(ls|shuf -n1)

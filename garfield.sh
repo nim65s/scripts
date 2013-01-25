ad8eb69e0974b65ebeee874a77c469be5cfed67e
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-01-25 10:37:59 +0100

    garfield: passage de feh à eog pour la gestion de la taille du fullscreen

--- garfield.sh
+++ garfield.sh
@@ -4,6 +4,6 @@ cd $HOME/images
 
 [[ -e garfield.gif ]] && rm garfield.gif
 wget -q "http://picayune.uclick.com/comics/ga/$(date '+%Y' --date 'yesterday')/ga$(date '+%y%m%d' --date 'yesterday').gif" -O garfield.gif
-feh -ZF garfield.gif
+eog -f garfield.gif
 
 exit 0

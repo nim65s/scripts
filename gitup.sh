2fa52c681eba02143b4d27ce263acb5f212954f6
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-06-03 02:28:02 +0200

    le status doit être hors du [[ -f .gitmodules]] …

--- gitup.sh
+++ gitup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while read repo
-do (( cd $repo && git pull && [[ -f .gitmodules ]] && git submodule foreach git pull && git status|| true ) | grep -v 'Already up-to-date.\|nothing to commit, working directory clean\|# On branch master\|^Entering' ) &
+do (( cd $repo && git pull && [[ -f .gitmodules ]] && git submodule foreach git pull || true ; git status) | grep -v 'Already up-to-date.\|nothing to commit, working directory clean\|# On branch master\|^Entering' ) &
 done < ~/.gitrepos
 
 wait

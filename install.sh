0b06e0e3e06c708ffe7a66f9634bd8564fec3ec1
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-06-25 15:37:23 +0000

    install.sh fonctionnel

--- install.sh
+++ install.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 # curl https://raw.github.com/nim65s/scripts/master/install.sh | bash
 
-PACKAGES="git vim zsh"
-which pacman && sudo pacman -Syu --noconfirm $PACKAGES
-which apt-get && sudo apt-get install $PACKAGES
+which pacman 2> /dev/null && sudo pacman -Syu --noconfirm git gvim zsh openssh
+which apt-get 2> /dev/null && sudo apt-get install git zsh vim-gnome
 
-for repo in dotfiles script
+mkdir ~/.ssh
+scp saurelg@ssh.inpt.fr:.ssh/id_rsa .ssh
+
+ssh-agent -s > ~/.ssh/tmpagent
+. ~/.ssh/tmpagent
+ssh-add
+
+for repo in dotfiles scripts
 do
-    git clone https://github.com/nim65s/$repo.git
+    git clone git@github.com:nim65s/$repo.git
     cd $repo
     git submodule init
     git submodule update
     git submodule foreach git checkout master
-    cd ..
+    cd
 done
 
-for file in .bash_profile .bash_logout .tmux.conf .nanorc .xbindkeyrc .vimpagerrc .vimrc .Xdefaults .gitconfig .bashrc .hgrc .zshrc .xmonad
+rm .ssh/id_rsa
+
+for file in .bash_profile .bash_logout .tmux.conf .nanorc .xbindkeyrc .vimpagerrc .vimrc .Xdefaults .gitconfig .bashrc .hgrc .zshrc .xmonad .vim
 do
     ln -s dotfiles/$file
 done
 
-for files in dotfiles/.config/*
+for files in awesome dfc fish ipython pep8 ranger terminator zathura
 do
-    ln -s $files .config
+    ln -s dotfiles/.config/$files .config
 done

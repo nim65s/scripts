eca1a3a9113a5329dacd0832c6139c7070f0616a
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-11-24 19:49:50 +0100

    gitup ignore les messages bateaux de --rebase

--- gitup.sh
+++ gitup.sh
@@ -18,6 +18,7 @@ while read repo ; do
         ( cd $repo && git pull --rebase 2>&1 && [[ -f .gitmodules ]] && git submodule foreach git pull || true ; git status) >> $TEMP
         sed -i '/up[- ]to[- ]date.$/d;/^nothing to commit/d;/^# On branch /d;/^Entering/d;/^ControlSocket/d' $TEMP
         sed -i '/^# Sur la branche/d;/^rien à valider, la copie de travail est propre/d;/^La branche courante master est à jour.$/d' $TEMP
+        sed -i '/mise à jour en avance rapide sur/d;/^Premièrement, retour de head pour rejouer votre travail par-dessus.../d' $TEMP
     ) &
 done < ~/.gitrepos
 

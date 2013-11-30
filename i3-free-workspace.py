39b8cd56de174fce050857aa53ffa593ec34b579
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-11-30 19:04:02 +0100

    i3-free-workspace.py can switch or move container to next or prev free workspace

--- i3-free-workspace.py
+++ i3-free-workspace.py
@@ -5,6 +5,15 @@ import sys
 
 import i3
 
+argv = sys.argv
+
+if argv[1] not in ['switch', 'move']:
+    print "first arg must be switch or move"
+    sys.exit(1)
+if argv[2] not in ['prev', 'next']:
+    print "second arg must be prev or next"
+    sys.exit(2)
+
 
 def focused(workspace):
     if workspace['focused']:
@@ -14,17 +23,16 @@ workspaces = i3.get_workspaces()
 current_ws = filter(focused, workspaces)[0]['num']
 
 ws_list = range(current_ws + 1, 11) + range(1, current_ws)
-print ws_list
 
-if len(sys.argv) > 1:
+if argv[2] == 'prev':
     ws_list.reverse()
 
 used_ws = [ws['num'] for ws in i3.get_workspaces()]
-print used_ws
 
 for ws in ws_list:
-    print ws
     if ws not in used_ws:
-        print ws
-        i3.command('workspace', str(ws))
+        if argv[1] == 'switch':
+            i3.command('workspace', str(ws))
+        else:
+            i3.command('move container to workspace', str(ws))
         break

d09bc6d9cb5fa8d9e761b322c5d4ba331d6cc5a4
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-12-09 09:31:16 +0100

    gitup: nouveaux messages & couleur

--- gitup.sh
+++ gitup.sh
@@ -13,19 +13,19 @@ while read repo ; do
     (
         TEMP=$(mktemp)
         echo $TEMP >> $MAINTEMP
-        echo > $TEMP
-        echo $repo >> $TEMP
+        echo -e "\033[1;32m $repo \033[0m" > $TEMP
         ( cd $repo && git pull --rebase 2>&1 && [[ -f .gitmodules ]] && git submodule foreach git pull || true ; git status) >> $TEMP
         sed -i '/up[- ]to[- ]date.$/d;/^nothing to commit/d;/^# On branch /d;/^Entering/d;/^ControlSocket/d' $TEMP
         sed -i '/^# Sur la branche/d;/^rien à valider, la copie de travail est propre/d;/^La branche courante master est à jour.$/d' $TEMP
         sed -i '/mise à jour en avance rapide sur/d;/^Premièrement, retour de head pour rejouer votre travail par-dessus.../d' $TEMP
+        sed -i '/^Your branch is up-to-date with/d;/^On branch /d;/^$/d' $TEMP
     ) &
 done < ~/.gitrepos
 
 wait
 
 while read temp ; do
-    [[ $(wc -l $temp | cut -d' ' -f1) -gt 2 ]] && cat $temp
+    [[ $(wc -l $temp | cut -d' ' -f1) -gt 1 ]] && cat $temp
     rm $temp
 done < $MAINTEMP
 

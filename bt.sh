94e6147e9c704155594543e1327edad95f4ebbfd
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-04-22 13:02:09 +0900

    second jet de yet another un truc qui permet de se réveiller le matin → power on device

--- bt.sh
+++ bt.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [[ $(qdbus --system org.bluez /org/bluez/hci0/dev_00_18_09_1E_FC_BB org.bluez.Device1.Connected) = "false" ]]
+then
+    dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0 org.freedesktop.DBus.Properties.Set string:"org.bluez.Adapter1" string:"Powered" variant:boolean:true
+fi
+
 while [[ $(qdbus --system org.bluez /org/bluez/hci0/dev_00_18_09_1E_FC_BB org.bluez.Device1.Connected) = "false" && $(acpi -a) = 'Adapter 0: on-line' ]]
 do
     dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0/dev_00_18_09_1E_FC_BB org.bluez.Device1.Connect 2> /dev/null
 done
 
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause > /dev/null
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play > /dev/null

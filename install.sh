324ae77692e24038635bdff0dc66c60cbbb099f8
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-04-22 16:13:44 +0900

    install: liens de i3 & xinitrc

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ which pacman 2> /dev/null && sudo pacman -Syu --noconfirm git gvim zsh openssh
 which apt-get 2> /dev/null && yes|sudo apt-get install git zsh vim-gnome
 which yum 2> /dev/null && yes|sudo yum install git zsh vim
 
-mkdir ~/.ssh
+mkdir -p ~/.ssh
 scp saurelg@ssh.inpt.fr:.ssh/id_rsa .ssh
 
 ssh-agent -s > ~/.ssh/tmpagent
@@ -27,7 +27,7 @@ done
 
 rm .ssh/id_rsa
 
-for file in .bash_profile .bash_logout .tmux.conf .nanorc .xbindkeyrc .vimpagerrc .vimrc .Xdefaults .gitconfig .bashrc .hgrc .zshrc .xmonad .vim
+for file in .bash_profile .bash_logout .tmux.conf .nanorc .xbindkeyrc .vimpagerrc .vimrc .Xdefaults .gitconfig .bashrc .hgrc .zshrc .xmonad .vim .i3 .xinitrc
 do
     rm -f $file
     ln -s dotfiles/$file

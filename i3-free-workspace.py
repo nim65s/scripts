5b9af9e14c52bc962d53e3ba109ad2ce91224ac3
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-04-23 22:51:24 +0900

    i3-free-workspace.py: argparse

--- i3-free-workspace.py
+++ i3-free-workspace.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python2
-# -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
-
-import sys
+import argparse
 
 import i3
 
-argv = sys.argv
+parser = argparse.ArgumentParser(description='switch to or move container to next or prev free workspace')
+parser.add_argument('action', nargs=1, choices=['switch', 'move'])
+parser.add_argument('direction', nargs=1, choices=['prev', 'next'])
 
-if argv[1] not in ['switch', 'move']:
-    print "first arg must be switch or move"
-    sys.exit(1)
-if argv[2] not in ['prev', 'next']:
-    print "second arg must be prev or next"
-    sys.exit(2)
+args = parser.parse_args()
 
 
 def focused(workspace):
@@ -26,7 +20,7 @@ current_ws = filter(focused, workspaces)[0]['num']
 
 ws_list = range(current_ws + 1, 11) + range(1, current_ws)
 
-if argv[2] == 'prev':
+if args.direction[0] == 'prev':
     ws_list.reverse()
 
 used_ws = [ws['num'] for ws in i3.get_workspaces()]
@@ -36,7 +30,7 @@ try:
 except StopIteration:
     sys.exit(3)
 
-if argv[1] == 'switch':
+if args.action[0] == 'switch':
     i3.command('workspace', str(ws))
 else:
     i3.command('move container to workspace', str(ws))

15f69a23aff4693c3cbcd66b3b8c945f966cd961
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-04-23 15:45:32 +0900

    nouveau workaround: c’est juste le Play de spotify qui marche pas → Pause puis PlayPause…

--- bt.sh
+++ bt.sh
@@ -10,4 +10,5 @@ do
     dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0/dev_00_18_09_1E_FC_BB org.bluez.Device1.Connect 2> /dev/null
 done
 
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play > /dev/null
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause > /dev/null
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause > /dev/null

9d4eb7e36cee23e29cce717549d8e0cdfc068587
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-04-24 12:11:01 +0900

    changement de casque BT

--- bt.sh
+++ bt.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [[ $(qdbus --system org.bluez /org/bluez/hci0/dev_00_18_09_1E_FC_BB org.bluez.Device1.Connected) = "false" ]]
+if [[ $(qdbus --system org.bluez /org/bluez/hci0/dev_00_0C_8A_67_96_90 org.bluez.Device1.Connected) = "false" ]]
 then
     dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0 org.freedesktop.DBus.Properties.Set string:"org.bluez.Adapter1" string:"Powered" variant:boolean:true
 fi
 
-while [[ $(qdbus --system org.bluez /org/bluez/hci0/dev_00_18_09_1E_FC_BB org.bluez.Device1.Connected) = "false" && $(acpi -a) = 'Adapter 0: on-line' ]]
+while [[ $(qdbus --system org.bluez /org/bluez/hci0/dev_00_0C_8A_67_96_90 org.bluez.Device1.Connected) = "false" && $(acpi -a) = 'Adapter 0: on-line' ]]
 do
-    dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0/dev_00_18_09_1E_FC_BB org.bluez.Device1.Connect 2> /dev/null
+    dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0/dev_00_0C_8A_67_96_90 org.bluez.Device1.Connect 2> /dev/null
 done
 
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause > /dev/null

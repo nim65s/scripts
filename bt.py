ba86497c6dcbe7fa2668587d7eaf32694f5de2d2
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-04-24 20:00:09 +0900

    passage de bt.sh à bt.py

---
+++ bt.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from dbus import Boolean, Interface, SystemBus
+
+DEVICE = '00:0C:8A:67:96:90'
+DEVICE = DEVICE.replace(':', '_')
+
+bus = SystemBus()
+
+proxy_power = bus.get_object('org.bluez', '/org/bluez/hci0')
+interface_power = Interface(proxy_power, dbus_interface='org.freedesktop.DBus.Properties')
+
+if not interface_power.Get('org.bluez.Adapter1', 'Powered'):
+    interface_power.Set('org.bluez.Adapter1', 'Powered', True)
+
+proxy_connected = bus.get_object('org.bluez', '/org/bluez/hci0/dev_{}'.format(DEVICE))
+interface_connected = Interface(proxy_connected, dbus_interface='org.freedesktop.DBus.Properties')
+
+if not interface.Get('org.bluez.Device1', 'Connected'):
+    interface_connect = Interface(proxy_connected, dbus_interface='org.bluez.Device1')
+    interface_connect.Connect()

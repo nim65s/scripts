380a9cf7365f9bff12cb57d12673a827bc63c124
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-07-04 11:51:33 +0200

    it works \o/

--- croptimg.sh
+++ croptimg.sh
@@ -2,17 +2,16 @@
 
 which optipng || exit 1
 which jpegoptim || exit 2
-test -f ~/.croptimg || exit 3
 
-while read d
+for d in /var/www/*/{media,static}
 do
-    for i in $(find . -iname \*.png)
+    for i in $(find $d -iname \*.png)
     do
         optipng "$i"
     done
 
-    for i in $(find . -iname \*.jpg)
+    for i in $(find $d -iname \*.jpg)
     do
         jpegoptim --strip-all "$i"
     done
-done < ~/.croptimg
+done

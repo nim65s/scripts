2c8d7009d7abb2298a402781d11900bc12288fb1
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-08-20 14:23:52 +0900

    wait.py & bt waits

--- bt.py
+++ bt.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python
 
 from dbus import Boolean, Interface, SystemBus
+from dbus.exceptions import DBusException
+from wait import wait
 
 DEVICE = '00:0C:8A:67:96:90'
 DEVICE = DEVICE.replace(':', '_')
+
+index = 0
 
 bus = SystemBus()
 
 proxy_power = bus.get_object('org.bluez', '/org/bluez/hci0')
 interface_power = Interface(proxy_power, dbus_interface='org.freedesktop.DBus.Properties')
 
-if not interface_power.Get('org.bluez.Adapter1', 'Powered'):
+while not interface_power.Get('org.bluez.Adapter1', 'Powered'):
     interface_power.Set('org.bluez.Adapter1', 'Powered', True)
+    index = wait(index, text='powering')
 
 proxy_connected = bus.get_object('org.bluez', '/org/bluez/hci0/dev_{}'.format(DEVICE))
 interface_connected = Interface(proxy_connected, dbus_interface='org.freedesktop.DBus.Properties')
 
-if not interface_connected.Get('org.bluez.Device1', 'Connected'):
+while not interface_connected.Get('org.bluez.Device1', 'Connected'):
     interface_connect = Interface(proxy_connected, dbus_interface='org.bluez.Device1')
-    interface_connect.Connect()
+    try:
+        interface_connect.Connect()
+        index = wait(index, text='connecting')
+    except DBusException as e:
+        index = wait(index, text=e.get_dbus_message())
+
---
+++ wait.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from fcntl import ioctl
+from struct import unpack
+from sys import stdout
+from termios import TIOCGWINSZ
+from time import sleep
+
+
+def wait(index=0, sleep_time=0.25, symbols=['→', '↘', '↓', '↙', '←', '↖', '↑', '↗'], text=""):
+    width = unpack('HH', ioctl(stdout.fileno(), TIOCGWINSZ, '0000'))[1]
+    stdout.write('\r%s %s%s' % (text, symbols[index], ' ' * (width - len(text) - 3)))
+    stdout.flush()
+    sleep(sleep_time)
+    return (index + 1) % len(symbols)
+
+if __name__ == '__main__':
+    index = 0
+    for i in range(25):
+        index = wait(index, text='waiting…')

8c21da5c74943a1e47a5f6c1d5531ba2b31ea3a2
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-09-06 16:45:01 +0900

    guignols: options pour dl et/ou lire/rm

--- guignols.sh
+++ guignols.sh
@@ -4,18 +4,36 @@ mkdir -p ~/guignols
 cd ~/guignols
 touch ~/.guignols
 
-curl 'http://www.canalplus.fr/c-divertissement/pid1784-c-les-guignols.html' | grep 'c-les-guignols.html?vid=' | tr ' ' '\n' | grep '^href' | cut -d'"' -f2 | sort | uniq > new
-diff ~/.guignols new | grep '>' | cut -d' ' -f2 > diff
+function download {
+    curl 'http://www.canalplus.fr/c-divertissement/pid1784-c-les-guignols.html' | grep 'c-les-guignols.html?vid=' | tr ' ' '\n' | grep '^href' | cut -d'"' -f2 | sort | uniq > new
+    diff ~/.guignols new | grep '>' | cut -d' ' -f2 > diff
 
-while read url
-do youtube-dl $url &
-done < diff
+    while read url
+    do youtube-dl $url &
+    done < diff
 
-wait
+    wait
 
-cat new ~/.guignols | sort | uniq > old
-mv old ~/.guignols
-rm new diff
+    cat new ~/.guignols | sort | uniq > old
+    mv old ~/.guignols
+    rm new diff
 
-rm -f "La semaine des Guignols - Semaine du "* 2> /dev/null
-rm -f "Les Guignols du "* 2> /dev/null
+    rm -f "La semaine des Guignols - Semaine du "* 2> /dev/null
+    rm -f "Les Guignols du "* 2> /dev/null
+}
+
+function watch {
+    [[ -f * ]] && vlc $(ls | sort)
+    echo -n "Tout supprimer ? [O/n] → "
+    read -N 1 del
+    if [[ $del != n && $del != N ]]
+    then rm * 2> /dev/null
+    fi
+}
+
+if [[ $1 == dl ]]
+then download
+elif [[ $1 == watch ]]
+then watch
+else download && watch
+fi

00ceee5ed38c6a5a0f172bb348e64f3ed4a1d7e5
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-12-11 11:54:34 +0100

    gitup s’occupe des sous-modules

--- gitup.sh
+++ gitup.sh
@@ -10,20 +10,25 @@
 MAINTEMP=$(mktemp)
 
 while read repo ; do
-    if [[ $repo == -* ]]
-    then
+    if [[ $repo == -* ]] ; then
         echo $repo
     else
         (
             TEMP=$(mktemp)
             echo $TEMP >> $MAINTEMP
-            echo -e "\033[1;32m $repo \033[0m" > $TEMP
-            ( cd $repo && git pull --rebase 2>&1 && [[ -f .gitmodules ]] && git submodule foreach git pull || true ; git status) >> $TEMP
-            sed -i '/up[- ]to[- ]date.$/d;/^nothing to commit/d;/^# On branch /d;/^Entering/d;/^ControlSocket/d' $TEMP
-            sed -i '/^# Sur la branche/d;/^rien à valider, la copie de travail est propre/d;/^La branche courante master est à jour.$/d' $TEMP
-            sed -i '/mise à jour en avance rapide sur/d;/^Premièrement, retour de head pour rejouer votre travail par-dessus.../d' $TEMP
-            sed -i '/^Votre branche est à jour avec /d;/^Sur la branche /d;/^$/d;/Entrée dans/d' $TEMP
-            sed -i '/^Your branch is up-to-date with/d;/^On branch /d;/^$/d' $TEMP
+            echo -e "\033[1;32m $repo \033[0m" >> $TEMP
+            cd $repo || exit 1
+            git pull --rebase origin master >> $TEMP 2> /dev/null
+            if [[ -f .gitmodules ]] ; then
+                git submodule init >> $TEMP 2> /dev/null
+                git submodule update >> $TEMP 2> /dev/null
+                git submodule foreach git checkout master >> $TEMP 2> /dev/null
+                git submodule foreach git pull --rebase origin master >> $TEMP 2> /dev/null
+            fi
+            git status >> $TEMP
+            sed -i "/Sur la branche master/d;/Votre branche est à jour avec 'origin\/master'./d" $TEMP
+            sed -i "/rien à valider, la copie de travail est propre/d;/^Entrée dans '/d" $TEMP
+            sed -i "/La branche courante master est à jour./d;/^Chemin de sous-module '/d" $TEMP
         ) &
     fi
 done < ~/.gitrepos

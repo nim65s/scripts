8dc1692199e71329f12953fe88954de5fdc95a0b
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-12-21 00:55:08 +0100

    unknown key…

--- gpg.sh
+++ gpg.sh
@@ -68,7 +68,7 @@ end
 
 gpg_table net7 (curl http://www.bde.inp-toulouse.fr/clubs/inp-net/contact.php|grep OpenPGP-Key|cut -d'"' -f 4)
 gpg_table CdL14 089047FE 382A5C4D 4653CF28 552CF98B 5F4445B5 682A3916 6B17EA1E 72F93B05 78758817 C2AA477E DD999172 F3B2CEDE
-gpg_table fosdem15 (curl https://ksp.fosdem.org/keys/|grep unknown|cut -d'"' -f8|sed 's/........//'|grep -v 132B94A6)
+gpg_table fosdem15 (curl https://ksp.fosdem.org/keys/|grep unknown|cut -d'"' -f8|sed 's/........//'|grep -v '132B94A6\|92126B54')
 
 scp {net7,CdL14,fosdem15}.{html,txt} n7:www_public/pgp
 rm -vf {net7,CdL14,fosdem15}.{html,txt}

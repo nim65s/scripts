2f2c8082db3810f9e632d7d1c85a0f4972748e47
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-06-09 18:48:10 +0200

    authorized key

--- install.sh
+++ install.sh
@@ -27,6 +27,8 @@ then
     RM_ID_RSA=true
 fi
 
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDO1DwbWEyl9W9+VxqaIUH4XPVoKMpoxcyh2X9/1NcpTtMmkEwxAfQDONp0R8n4HGc7YpUUFQ0PgjIqhXaOz5zvWwr2wIf+1tPdV3lpxxNawZ8iAwwApaPqLdR0o0NAM8hKTbxB3ObVuzN5T5VJO/r4j1G4kH0wEiOrPnph5LPvkwGlMyIV8B7948v/CAe/YsTQA7jq6oijOAU/MTpjWjXANcDj688IgrDobx9L0T1oAhVrs11SqofrWuWTbgofLIR4mQhbm7t2DXha4kzD82lB9ia6TAXG9mysGsBYkIl2RZ9BA8Ax5ftou1zbtpYyO1SN5hytBi07BsYep/tHCSKn nim@Nausicaa' >> .ssh/authorized_keys
+
 for repo in dotfiles scripts
 do
     rm -rf $HOME/$repo

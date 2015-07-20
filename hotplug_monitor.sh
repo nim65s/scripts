40e43afcdd607fa81acebc1bed67f7a0cd86d69a
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-07-20 09:29:33 +0200

    HDMI1

--- hotplug_monitor.sh
+++ hotplug_monitor.sh
@@ -3,6 +3,10 @@
 if $(xrandr | grep -q "VGA1 connected")
 then
     xrandr --output VGA1 --auto --right-of LVDS1
+elif $(xrandr | grep -q "HDMI1 connected")
+then
+    xrandr --output HDMI1 --auto --right-of LVDS1
 else
     xrandr --output VGA1 --off
+    xrandr --output HDMI1 --off
 fi

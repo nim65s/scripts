9c01f6e872d90885195c8631b9e2429d4ed32617
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-10-07 19:53:56 +0200

    demunim: imports if necessary

--- demunim.py
+++ demunim.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
 import shelve
-from os import environ
 from os.path import expanduser
-from pathlib import Path
-from stat import S_IEXEC, S_IXGRP, S_IXOTH
 from subprocess import PIPE, run
 
 with shelve.open(expanduser('~/.cache/demunim')) as shelf:
-    if 'cmds' not in shelf:
-        x = S_IEXEC | S_IXGRP | S_IXOTH
+    if 'cmds' not in shelf:  # First Launch, get the list of all possible commands
+        from os import environ
+        from pathlib import Path
+        from stat import S_IEXEC, S_IXGRP, S_IXOTH
+        x = S_IEXEC | S_IXGRP | S_IXOTH  # executable files
         path = (Path(d) for d in environ['PATH'].split(':'))
         shelf['cmds'] = sorted([f.name for d in path for f in d.iterdir() if f.is_file() and f.stat().st_mode & x])
-    out = run(['dmenu'], input='\n'.join(shelf['cmds']), stdout=PIPE, universal_newlines=True).stdout.strip()
+    out = run('dmenu', input='\n'.join(shelf['cmds']), stdout=PIPE, universal_newlines=True).stdout.strip()
     if out:
         if out in shelf['cmds']:
             shelf['cmds'].remove(out)

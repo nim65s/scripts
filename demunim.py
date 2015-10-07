d0e5d0a0adaa17ccb654db335d9247098dffeb5b
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-10-07 18:42:03 +0200

    demunim: peewee

--- demunim.py
+++ demunim.py
@@ -1,72 +1,39 @@
 #!/usr/bin/env python
 
 from datetime import datetime
-from distutils.spawn import find_executable
 from os import environ
 from os.path import expanduser
 from pathlib import Path
 from stat import S_IEXEC, S_IXGRP, S_IXOTH
 from subprocess import PIPE, run
-from sys import argv
 
-from sqlalchemy import Column, DateTime, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.sql import exists
+from peewee import CharField, DateTimeField, Model, SqliteDatabase
 
-Base = declarative_base()
+db = SqliteDatabase(str(Path(environ['XDG_CACHE_HOME'] if 'XDG_CACHE_HOME' in environ else expanduser('~/.cache')) / 'demunim'))
 
 
-class Command(Base):
-    __tablename__ = 'command'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
-    datetime = Column(DateTime)
+class Command(Model):
+    name = CharField(unique=True)
+    date = DateTimeField(null=True)
 
-    def __str__(self):
-        return self.name
-
-
-def setup_database():
-    db = Path(environ['XDG_CACHE_HOME'] if 'XDG_CACHE_HOME' in environ else expanduser('~/.cache')) / 'demunim.sqlite'
-    if not db.parent.is_dir():
-        db.parent.mkdir(parents=True)
-    engine = create_engine('sqlite:///%s' % db)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-    return Session()
-
-
-def get_executables():
-    x = S_IEXEC | S_IXGRP | S_IXOTH
-    return (f.name for d in environ['PATH'].split(':') for f in Path(d).iterdir() if f.is_file() and f.stat().st_mode & x)
-
-
-def populate_database(session):
-    for command in session.query(Command):
-        if find_executable(command.name) is None:
-            session.delete(command)
-    for command in get_executables():
-        if not session.query(exists().where(Command.name == command)).scalar():
-            session.add(Command(name=command))
-    session.commit()
-
+    class Meta:
+        database = db
 
 if __name__ == '__main__':
-    session = setup_database()
+    db.connect()
+    if not Command.table_exists():
+        db.create_table(Command)
+        with db.atomic():
+            for d in environ['PATH'].split(':'):
+                for f in Path(d).iterdir():
+                    if f.is_file() and f.stat().st_mode & (S_IEXEC | S_IXGRP | S_IXOTH):
+                        Command.create(name=f.name)
 
-    if len(argv) > 1:
-        populate_database(session)
-    else:
-        executables = (c.name for c in session.query(Command).order_by(Command.datetime.desc()).all())
-        out = run(['dmenu'], input='\n'.join(executables), stdout=PIPE, universal_newlines=True).stdout.strip()
-        if out:
-            command = session.query(Command).filter(Command.name == out).first()
-            if command is None:
-                command = Command(name=out)
-                session.add(command)
-            command.datetime = datetime.now()
-            session.commit()
+    executables = (c.name for c in Command.select().order_by(Command.date.desc()))
+    out = run(['dmenu'], input='\n'.join(executables), stdout=PIPE, universal_newlines=True).stdout.strip()
+    if out:
+        command = Command.get_or_create(name=out)[0]
+        command.date = datetime.now()
+        command.save()
 
-            run(out)
+        run(out)

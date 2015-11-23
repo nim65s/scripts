fdf80ede70fa724125968110c91dabee99071d39
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-11-23 16:17:45 +0100

    Makefile génériques

--- generateTexMakefile.sh
+++ generateTexMakefile.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 
-[[ -f Makefile ]] && rm -v Makefile
-[[ -f Makefile.quick ]] && rm -v Makefile.quick
 
-$(grep -q xunicode *.tex) && TEX="xelatex" || TEX="pdflatex"
+ls | grep -q .tex || exit 1
 
-echo -n "all: " >> Makefile
-for FILE in *.tex
-do echo -n "$(echo "$FILE " | grep -v '*' | sed 's/\.tex/.pdf/')" >> Makefile
+echo -n all: > Makefile
+
+for file in *.tex
+do echo -n " ${file/.tex/.pdf}" >> Makefile
 done
+
+echo >> Makefile
 echo >> Makefile
 
 echo '%.pdf: %.tex' >> Makefile
 echo -en "\t" >> Makefile
-if [[ $1 != *m* ]] 
-then echo "$TEX -shell-escape $<" >> Makefile
-else echo "(( $TEX -shell-escape $< || ( rm \$@ && false )) && $TEX -shell-escape $< || ( rm \$@ && false )) && $TEX -shell-escape $<" >> Makefile
-fi
+echo 'latexmk -pdf -pdflatex="$(shell grep -q xunicode $< && echo xe || echo pdf)latex" $<' >> Makefile
+
 echo >> Makefile
+
 echo 'clean:' >> Makefile
 echo -en "\t" >> Makefile
-echo '-rm -vf *.aux *.log *.nav *.out *.snm *.toc *.tmp *.tns *.pyg *.vrb *~ *.orig *.gnuplot *.table 2> /dev/null' >> Makefile
+echo '-rm -vf *.aux *.log *.nav *.out *.snm *.toc *.tmp *.tns *.pyg *.vrb *~ *.orig *.gnuplot *.table *.fls *.fdb_latexmk *.blg 2> /dev/null' >> Makefile
 
-[[ $1 == *m* ]] && sed "s/(( $TEX -shell-escape $< || ( rm \$@ && false )) && $TEX -shell-escape $< || ( rm \$@ && false )) && $TEX -shell-escape $</$TEX -shell-escape $</" Makefile > Makefile.quick
-
-echo "---------Makefile---------"
 cat Makefile
-if [[ $1 == *m* ]]
-then
-    echo "------Makefile.quick------"
-    cat Makefile.quick
-fi

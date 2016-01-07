a77f3686f079be8a6f6e4cc4358cc2b5ab5b09ba
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-01-07 18:17:20 +0100

    clean

--- pandoc-videos.py
+++ pandoc-videos.py
@@ -7,27 +7,26 @@ to html5 & latex video tags
 
 from pandocfilters import toJSONFilter, RawBlock
 
-WEB = ['revealjs', 'html', 'html5']
-TEX = ['beamer', 'latex']
-
-TEMPLATE_WEB = r'''
-<video controls>
-  <source src="%s" type="video/mp4">
-  Your browser does not support mp4 in html5's video tag.
-</video>
-'''
-
-TEMPLATE_TEX = r'\movie[width=8cm,height=4.5cm]{}{%s}'
+FORMATS = {
+        'latex': ['beamer', 'latex'],
+        'html': ['revealjs', 'html', 'html5'],
+        }
+TEMPLATES = {
+        'latex': r"\movie[width=8cm,height=4.5cm]{}{%s}",
+        'html': r"<video controls><source src='%s' type='video/mp4'></video>",
+        }
+PERCENT = {
+        't': 'Str',
+        'c': '%',
+        }
 
 
 def media(key, value, format, meta):
-    if key == 'Para' and value[0] == {'t': 'Str', 'c': '%'} and value[1]['t'] == 'Link':
+    if key == 'Para' and value[0] == PERCENT and value[1]['t'] == 'Link':
         src = value[1]['c'][2][0]
-        if src.endswith('.mp4'):
-            if format in WEB:
-                return [RawBlock('html', TEMPLATE_WEB % src)]
-            elif format in TEX:
-                return [RawBlock('latex', TEMPLATE_TEX % src)]
+        for fk, fv in FORMATS.items():
+            if format in fv:
+                return [RawBlock(fk, TEMPLATES[fk] % src)]
 
 if __name__ == "__main__":
     toJSONFilter(media)

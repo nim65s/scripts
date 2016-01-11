c97d2186185213853419f12f8df39a8c93af32d8
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-01-11 14:12:48 +0100

    figures, captions

--- pandoc-videos.py
+++ pandoc-videos.py
@@ -8,14 +8,17 @@ Same syntax as https://github.com/rekado/parkdown#extensions
 """
 
 from pandocfilters import RawBlock, toJSONFilter
-
 FORMATS = {
         'latex': ['beamer', 'latex'],
         'html': ['revealjs', 'html', 'html5'],
         }
 TEMPLATES = {
-        'latex': r"\centering\movie[width=8cm,height=4.5cm]{}{%s}",
-        'html': r"<video controls><source src='%s' type='video/mp4'></video>",
+        'latex': r'''\begin{figure}[htbp]
+\centering
+\movie[width=8cm,height=4.5cm]{}{%s}
+\caption{%s}
+\end{figure}''',
+        'html': r"<video controls><source src='%s' type='video/mp4'>%s</video>",
         }
 PERCENT = {
         't': 'Str',
@@ -25,10 +28,10 @@ PERCENT = {
 
 def media(key, value, format, meta):
     if key == 'Para' and value[0] == PERCENT and value[1]['t'] == 'Link':
-        src = value[1]['c'][2][0]
+        title, src = value[1]['c'][1][0]['c'], value[1]['c'][2][0]
         for fmt_name, fmt_values in FORMATS.items():
             if format in fmt_values:
-                return [RawBlock(fmt_name, TEMPLATES[fmt_name] % src)]
+                return [RawBlock(fmt_name, TEMPLATES[fmt_name] % (src, title))]
 
 if __name__ == "__main__":
     toJSONFilter(media)

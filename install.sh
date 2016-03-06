fe22aac73e40928f94972b97632b24b401254a14
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-03-06 12:42:41 +0100

    install: VPNim, clean, reinstall check

--- install.sh
+++ install.sh
@@ -8,37 +8,38 @@
 
 cd
 
-mkdir -p .config .virtualenvs
+mkdir -p .config .virtualenvs .ssh .virtualenvs
+touch .gitrepos
 
-which pacman 2> /dev/null && sudo pacman -Syu --noconfirm git gvim fish openssh
-which apt-get 2> /dev/null && yes|sudo apt-get install git fish vim-gnome
-which yum 2> /dev/null && yes|sudo yum install git fish vim
+which pacman 2> /dev/null && sudo pacman -Syu --noconfirm git gvim fish openssh tinc
+which apt-get 2> /dev/null && sudo apt-get install git fish vim-gnome tinc
+which yum 2> /dev/null && sudo yum install git fish vim tinc
 
 RM_ID=false
 
-if [[ ! -d ~/.ssh || (! -f ~/.ssh/id_rsa  && ! -f ~.ssh/id_ed25519) ]]
+if [[ ! -d .ssh || (! -f .ssh/id_rsa  && ! -f .ssh/id_ed25519) ]]
 then
-    mkdir -p ~/.ssh
     scp saurelg@ssh.inpt.fr:.ssh/id_rsa .ssh
-
-    ssh-agent -s > ~/.ssh/tmpagent
-    . ~/.ssh/tmpagent
+    ssh-agent -s > .ssh/tmpagent
+    . .ssh/tmpagent
     ssh-add
     RM_ID=true
 fi
 
-echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDO1DwbWEyl9W9+VxqaIUH4XPVoKMpoxcyh2X9/1NcpTtMmkEwxAfQDONp0R8n4HGc7YpUUFQ0PgjIqhXaOz5zvWwr2wIf+1tPdV3lpxxNawZ8iAwwApaPqLdR0o0NAM8hKTbxB3ObVuzN5T5VJO/r4j1G4kH0wEiOrPnph5LPvkwGlMyIV8B7948v/CAe/YsTQA7jq6oijOAU/MTpjWjXANcDj688IgrDobx9L0T1oAhVrs11SqofrWuWTbgofLIR4mQhbm7t2DXha4kzD82lB9ia6TAXG9mysGsBYkIl2RZ9BA8Ax5ftou1zbtpYyO1SN5hytBi07BsYep/tHCSKn nim@Nausicaa' >> .ssh/authorized_keys
+NAUSICAA_KEY='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDO1DwbWEyl9W9+VxqaIUH4XPVoKMpoxcyh2X9/1NcpTtMmkEwxAfQDONp0R8n4HGc7YpUUFQ0PgjIqhXaOz5zvWwr2wIf+1tPdV3lpxxNawZ8iAwwApaPqLdR0o0NAM8hKTbxB3ObVuzN5T5VJO/r4j1G4kH0wEiOrPnph5LPvkwGlMyIV8B7948v/CAe/YsTQA7jq6oijOAU/MTpjWjXANcDj688IgrDobx9L0T1oAhVrs11SqofrWuWTbgofLIR4mQhbm7t2DXha4kzD82lB9ia6TAXG9mysGsBYkIl2RZ9BA8Ax5ftou1zbtpYyO1SN5hytBi07BsYep/tHCSKn nim@Nausicaa'
 
-for repo in dotfiles scripts
+grep -q $NAUSICAA_KEY .ssh/authorized_keys || echo $NAUSICAA_KEY >> .ssh/authorized_keys
+
+for repo in dotfiles scripts VPNim
 do
-    rm -rf $HOME/$repo
+    rm -rf $repo
     git clone git@github.com:nim65s/$repo.git --recursive
-    cd $repo
+    pushd $repo
     git submodule init
     git submodule update --recursive --remote --rebase
     git submodule foreach -q --recursive 'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
-    cd
-    grep -q $repo .gitrepos 2> /dev/null || echo $HOME/$repo >> .gitrepos
+    grep -q $repo ~/.gitrepos || pwd >> ~/.gitrepos
+    popd
 done
 
 $RM_ID && rm .ssh/{id_rsa,tmpagent}
@@ -55,7 +56,7 @@ do
     ln -s $HOME/dotfiles/.config/$files $HOME/.config/
 done
 
-mkdir -p $HOME/.virtualenvs
+rm -f $HOME/.virtualenvs/global_requirements.txt
 ln -s $HOME/dotfiles/global_requirements.txt $HOME/.virtualenvs/global_requirements.txt
 
 echo "chsh -s $(grep fish /etc/shells)"

2d23ea8feac9acec9f9473147c7fb3f7d10f1cad
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-03-21 10:32:00 +0100

    import_pgp.sh finds the encrypted message

--- import_pgp.sh
+++ import_pgp.sh
@@ -4,7 +4,8 @@ a=${1:-$(mktemp)}
 b=$(mktemp)
 
 vim $a
-gpg --decrypt $a > $b
-sed -n '/-----BEGIN PGP PUBLIC KEY BLOCK-----/,/-----END PGP PUBLIC KEY BLOCK-----/p' $b > $a
-gpg --import $a
+sed -n '/-----BEGIN PGP MESSAGE-----/,/-----END PGP MESSAGE-----/p' $a > $b
+gpg --decrypt $b > $a
+sed -n '/-----BEGIN PGP PUBLIC KEY BLOCK-----/,/-----END PGP PUBLIC KEY BLOCK-----/p' $a > $b
+gpg --import $b
 rm $a $b

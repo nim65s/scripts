5105178344ac8b9356a29658367266681cbfe40a
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-12-01 23:15:56 +0100

    web_spotify: first link on disambiguation

--- web_spotify.py
+++ web_spotify.py
@@ -6,6 +6,14 @@ import html2text
 import requests
 from bs4 import BeautifulSoup
 
+WIKIA = 'http://lyrics.wikia.com/'
+
+
+def get_lyrics(soup):
+    if not isinstance(soup, BeautifulSoup):
+        soup = BeautifulSoup(soup, 'html.parser')
+    return soup.find_all('div', class_='lyricbox')
+
 
 class SpotifyLocalHTTPClient(object):
     def __init__(self):
@@ -40,16 +48,20 @@ class SpotifyLocalHTTPClient(object):
 
     def lyrics(self):
         artist, song = self.track()
-        req = requests.get("http://lyrics.wikia.com/%s:%s" % (artist, song.split('-')[0].title()))
-        lyrics = BeautifulSoup(req.content, 'html.parser').find_all('div', class_='lyricbox')
+        req = requests.get("%s%s:%s" % (WIKIA, artist, song.split('-')[0].title()))
+        soup = BeautifulSoup(req.content, 'html.parser')
+        lyrics = get_lyrics(soup)
         if not lyrics:
-            search = BeautifulSoup(req.content, 'html.parser').find_all('a', class_='external text')
+            search = soup.find_all('a', class_='external text')
             if search:
                 req_s = requests.get(search[0].attrs['href'])
                 search_page = BeautifulSoup(req_s.content, 'html.parser').find_all('a', class_='result-link')
                 if search_page:
                     req_final = requests.get(search_page[0].attrs['href'])
-                    lyrics = BeautifulSoup(req_final.content, 'html.parser').find_all('div', class_='lyricbox')
+                    lyrics = get_lyrics(req_final.content)
+            elif 'disambiguation' in str(req.content):
+                disambiguation = requests.get(WIKIA + soup.find('div', id='mw-content-text').find('a').attrs['href'])
+                lyrics = get_lyrics(disambiguation.content)
         return '<h1>%s</h1><h2>%s</h2>%s' % (artist, song, str(lyrics)[1:-1] if lyrics else req.url)
 
 
@@ -60,4 +72,6 @@ if __name__ == '__main__':
         if (track, artist) != client.track():
             track, artist = client.track()
             print(html2text.html2text(client.lyrics()))
+            for _ in range(5):
+                print()
         sleep(1)

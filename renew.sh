995144c79629b840225408daf27fbc85d681da83
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-01-21 15:43:49 +0100

    letsencrypt renew

---
+++ renew.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# cron: letsencrypt renew --non-interactive --renew-hook /root/scripts/renew.sh
+# if XMPP certificate is renewed, we want to restart prosody
+
+grep -q 'im.saurel.me' <<< $RENEWED_DOMAINS && systemctl restart prosody

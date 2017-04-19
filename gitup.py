3662efa0f39f6b67d4c328bcb38a7ce83663cf0f
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-04-19 13:53:29 +0200

    gitup.py

--- gitup.py
+++ gitup.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from os import chdir
 from os.path import expanduser, isdir, isfile
-from subprocess import PIPE, STDOUT, run
+from subprocess import PIPE, STDOUT, Popen
 
 CONFIG = expanduser('~/.gitrepos')
 
 REMOVE = [
     '',
     'Déjà à jour.',
-    'Sur la branche master',
     'rien à valider, la copie de travail est propre',
+    'Premièrement, rembobinons head pour rejouer votre travail par-dessus...',
+    'Sur la branche master',
     "Votre branche est à jour avec 'origin/master'.",
     'La branche courante master est à jour.',
-    'Premièrement, rembobinons head pour rejouer votre travail par-dessus...',
     'master mise à jour en avance rapide sur refs/remotes/origin/master.',
+    "Déjà sur 'master'",
 ]
 
 
@@ -24,28 +24,31 @@ def title(string):
     return f"\033[1;32m {string} \033[0m"
 
 
+def run(cmds, repo):
+    return Popen(cmds, stderr=STDOUT, stdout=PIPE, cwd=repo, universal_newlines=True).stdout.read().split('\n')
+
+
 def gitup(repo):
     if repo.startswith('-'):
         return
     if not isdir(repo):
         raise ValueError(f'«{repo}» is not a directory')
-    chdir(repo)
     ret = [title(repo)]
     for cmds in [['git', 'fetch'], ['git', 'rebase']]:
-        ret += run(cmds, stderr=STDOUT, stdout=PIPE).stdout.decode().split('\n')
+        ret += run(cmds, repo)
     if isfile('.gitmodules'):
         for cmds in [['git', 'submodule', 'update', '--recursive', '--remote', '--rebase', '--init'],
                      ['git', 'submodule', 'foreach', '-q',
                       'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)']]:
-            ret += run(cmds, stderr=STDOUT, stdout=PIPE).stdout.decode().split('\n')
-    return ret + run(['git', 'status'], stderr=STDOUT, stdout=PIPE).stdout.decode().split('\n')
+            ret += run(cmds, repo)
+    return ret + run(['git', 'status'], repo)
 
 
 def clean(output):
     for rm in REMOVE:
         while rm in output:
             output.remove(rm)
-    if len(output) > 2:
+    if len(output) > 1:
         print('\n'.join(output))
 
 

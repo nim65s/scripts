67b3c49dfee377946aae43ad3627c82d86e1a384
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-06-16 16:32:47 +0200

    renew restarts prosody and nginx

--- renew.sh
+++ renew.sh
@@ -2,4 +2,5 @@
 # cron: letsencrypt renew --non-interactive --renew-hook /root/scripts/renew.sh
 # if XMPP certificate is renewed, we want to restart prosody
 
-systemctl restart $(grep -q 'im.saurel.me' <<< $RENEWED_DOMAINS && echo prosody || echo nginx)
+grep -q 'im.saurel.me' <<< $RENEWED_DOMAINS && systemctl restart prosody
+test -z "$RENEWED_DOMAINS" && systemctl restart nginx

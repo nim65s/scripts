9f33dd550771c2ef73f23bdd0c65e556afe00037
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-07-09 21:34:15 +0200

    check requirements can filter the repositories

--- check_requirements.py
+++ check_requirements.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 from os.path import expanduser
 
 import grequests
@@ -24,6 +25,7 @@ all_repos = []
 
 
 def show(package, repo, pypi):
+    " Show the version of {package} in {repo} wrt {pypi} "
     if package in repos[repo]:
         if repos[repo][package] == pypi:
             return "✔"
@@ -34,16 +36,22 @@ def show(package, repo, pypi):
 
 
 def dl(url_template, url_arguments, desc=''):
+    " Download in parallel {url_template} for each {url_arguments}, with a progress bar describing {desc}"
     all_requests = (grequests.get(url_template % arg, headers=HEADERS) for arg in url_arguments)
     yield from tqdm(grequests.imap(all_requests), desc=desc, total=len(url_arguments))
 
 
-for page in range(1000):
-    user_repos = requests.get(REPOS, {'per_page': 100, 'page': page}, headers=HEADERS).json()
-    if not user_repos:
-        break
-    all_repos += [repo['full_name'] for repo in user_repos if repo['full_name'] not in IGNORE]
+# Get the list of {all_repos} to consider.
+if len(sys.argv) > 1:
+    all_repos = sys.argv[1:]
+else:
+    for page in range(1000):
+        user_repos = requests.get(REPOS, {'per_page': 100, 'page': page}, headers=HEADERS).json()
+        if not user_repos:
+            break
+        all_repos += [repo['full_name'] for repo in user_repos if repo['full_name'] not in IGNORE]
 
+# Parse the requirements.txt file from each repo from {all_repos}
 for r in dl(REQUIREMENTS, all_repos, 'repositories'):
     if r.status_code == 200:
         full_name = '/'.join(r.url.split('/')[3:5])
@@ -59,10 +67,12 @@ for r in dl(REQUIREMENTS, all_repos, 'repositories'):
 all_packages = sorted(all_packages)
 all_repos = sorted(repos.keys())
 
+# Get the Pypi version of {all_packages}
 for r in dl(PyPI, all_packages, 'packages'):
     info = r.json()['info']
     pypi[info['name'].lower()] = info['version']
 
+# Print a everything in a table
 table = [["", ""] + [repo.split('/')[0] for repo in all_repos]]
 table += [["Package", "PyPI"] + [repo.split('/')[1] for repo in all_repos]]
 for package in all_packages:

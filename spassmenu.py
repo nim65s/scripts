de8d6ac1e250221277fb689585227d4b71d14bf5
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-09-08 23:00:14 +0200

    spassmenu.py

---
+++ spassmenu.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# sorted version of passmenu from password-store
+# TODO: use XDG_CACHE_DIR & PASSWORD_STORE_DIR
+
+import shelve
+import sys
+from os.path import expanduser
+from pathlib import Path
+from subprocess import PIPE, run
+
+SHELF = expanduser('~/.cache/spassmenu')
+PASSWORD_STORE_DIR = Path(expanduser('~/.password-store'))
+
+
+with shelve.open(SHELF) as shelf:
+    pwds = shelf['pwds'] if 'pwds' in shelf else []
+
+for filename in PASSWORD_STORE_DIR.glob('**/*.gpg'):
+    pwd = str(filename.relative_to(PASSWORD_STORE_DIR))[:-4]
+    if pwd not in pwds:
+        pwds.append(pwd)
+
+out = run(['dmenu'] + sys.argv[1:], input='\n'.join(pwds), stdout=PIPE, universal_newlines=True).stdout.strip()
+
+if out:
+    while out in pwds:
+        pwds.remove(out)
+    pwds = [out] + pwds
+
+    with shelve.open(SHELF) as shelf:
+        shelf['pwds'] = pwds
+
+    run(['pass', '-c', out])

fb6f1dd82b01f3ba508573ae21e1dff9c2b94a42
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-12-10 18:09:03 +0100

    almost done baff.sh

--- baff.sh
+++ baff.sh
@@ -3,7 +3,9 @@
 set -e
 #set -x
 
-SIGNER=${2:-9B1A79065D2F2B806C8A5A1C7D2ACDAF4653CF28}
+MSMTP_ACCOUNT=gandi
+
+SIGNER=9B1A79065D2F2B806C8A5A1C7D2ACDAF4653CF28
 SIGNER=$(gpg --list-keys --with-colon $SIGNER|grep '^pub'|cut -d ':' -f5)
 SIGNERPUBFILE=/tmp/baff_signer_pub_${SIGNER}.gpg
 SIGNERSECFILE=/tmp/baff_signer_sec_${SIGNER}.gpg
@@ -11,28 +13,34 @@ SIGNERSECFILE=/tmp/baff_signer_sec_${SIGNER}.gpg
 gpg --export $SIGNER > $SIGNERPUBFILE
 gpg --export-secret-keys $SIGNER > $SIGNERSECFILE
 
-KEY=${1:-FD86488777B1D56DFCF167BDF0FEAEF0093AA2CB}
-KEY=$(gpg --list-keys --with-colon $KEY|grep '^pub'|cut -d ':' -f5)
-KEYFILE=/tmp/baff_keyfile_${KEY}.gpg
-KEYRING=/tmp/baff_keyring_${KEY}.gpg
-
-
-echo
-echo "===== Signing $KEY ====="
-echo
-
-gpg --export $KEY > $KEYFILE
-gpg --no-default-keyring --keyring $KEYRING --import $KEYFILE $SIGNERPUBFILE $SIGNERSECFILE
-gpg --no-default-keyring --keyring $KEYRING --trusted-key $SIGNER -u $SIGNER --edit-key $KEY minimize save
-
-NUID=0
-while read uid
+for key in 7D2ACDAF4653CF28
 do
-    ((NUID++)) || true
-    echo "----- uid $NUID: $uid -----"
-    gpg --no-default-keyring --keyring $KEYRING --trusted-key $SIGNER -u $SIGNER --edit-key $KEY "uid $NUID" sign save
-    gpg --no-default-keyring --keyring $KEYRING --export --armor $KEY > ${KEYFILE}.${NUID}.signed_by.${SIGNER}.asc
-    #gpg --no-default-keyring --keyring $KEYRING -r $KEY --encrypt ${KEYFILE}.${NUID}.signed_by.${SIGNER}.asc
-done <<< $(gpg --with-colons --fingerprint $KEY|grep '^uid'|grep -v '^uid:r:'|cut -d':' -f10)
+    gpg --recv-keys $key
+    KEY=$(gpg --list-keys --with-colon $key|grep '^pub'|cut -d ':' -f5)
+    KEYFILE=/tmp/baff_keyfile_${KEY}.gpg
+    KEYRING=/tmp/baff_keyring_${KEY}.gpg
 
-rm $SIGNERPUBFILE $SIGNERSECFILE $KEYFILE $KEYRING
+    echo
+    echo "===== Signing $KEY ====="
+    echo
+
+    gpg --export $KEY > $KEYFILE
+    gpg --no-default-keyring --keyring $KEYRING --import $KEYFILE $SIGNERPUBFILE $SIGNERSECFILE
+    gpg --no-default-keyring --keyring $KEYRING --trusted-key $SIGNER -u $SIGNER --edit-key $KEY minimize save
+
+    NUID=0
+    while read uid
+    do
+        ((NUID++)) || true
+        echo "----- uid $NUID: $uid -----"
+        gpg --no-default-keyring --keyring $KEYRING --trusted-key $SIGNER -u $SIGNER --edit-key $KEY "uid $NUID" sign save
+        gpg --no-default-keyring --keyring $KEYRING --export --armor $KEY > ${KEYFILE}.${NUID}.signed_by.${SIGNER}.asc
+        #gpg --no-default-keyring --keyring $KEYRING -r $KEY --encrypt ${KEYFILE}.${NUID}.signed_by.${SIGNER}.asc
+        #cat | msmtp -a $MSMTP_ACCOUNT $uid
+
+    done <<< $(gpg --with-colons --fingerprint $KEY|grep '^uid'|grep -v '^uid:r:'|cut -d':' -f10)
+
+    rm $KEYFILE $KEYRING
+done
+
+rm $SIGNERPUBFILE $SIGNERSECFILE

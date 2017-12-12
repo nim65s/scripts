f476fd3ba0724ae997340e975725dc2323054538
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-12-12 17:27:46 +0100

    install: silent apt

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ mkdir -p .config .virtualenvs .ssh .virtualenvs .gnupg
 touch .gitrepos .ssh/authorized_keys
 
 which pacman 2> /dev/null && sudo pacman -Syu --noconfirm git gvim fish openssh tinc vimpager python-pip python2-pip rofi pass pcsc-tools ccid libusb-compat dunst
-which apt 2> /dev/null && sudo apt install gnupg2 terminator git fish vim-gnome tinc pcscd libpcsclite1 pcsc-tools scdaemon python-pip python3-pip
+which apt 2> /dev/null && sudo apt install -qqy gnupg2 terminator git fish vim-gnome tinc pcscd libpcsclite1 pcsc-tools scdaemon python-pip python3-pip
 which yum 2> /dev/null && sudo yum install git fish vim tinc
 
 echo enable-ssh-support > .gnupg/gpg-agent.conf

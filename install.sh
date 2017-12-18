5301f384c674d864c7483edad35f515f2ef75536
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-12-18 21:43:21 +0100

    msmtp-mta

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ cd
 mkdir -p .config .virtualenvs .ssh .virtualenvs .gnupg
 touch .gitrepos .ssh/authorized_keys
 
-which pacman 2> /dev/null && sudo pacman -Syu --noconfirm git gvim fish openssh tinc vimpager python-pip python2-pip rofi pass pcsc-tools ccid libusb-compat dunst
-which apt 2> /dev/null && sudo apt install -qqy gnupg2 terminator git fish vim-gnome tinc pcscd libpcsclite1 pcsc-tools scdaemon python-pip python3-pip
+which pacman 2> /dev/null && sudo pacman -Syu --noconfirm git gvim fish openssh tinc vimpager python-pip python2-pip rofi pass pcsc-tools ccid libusb-compat dunst msmtp-mta
+which apt 2> /dev/null && sudo apt install -qqy gnupg2 terminator git fish vim-gnome tinc pcscd libpcsclite1 pcsc-tools scdaemon python-pip python3-pip msmtp-mta
 which yum 2> /dev/null && sudo yum install git fish vim tinc
 
 echo enable-ssh-support > .gnupg/gpg-agent.conf

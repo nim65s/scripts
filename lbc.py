63d67ac87f2962cf462b8be8b0d3c8d678b89a9e
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-04-15 19:34:43 +0200

    lbc

---
+++ lbc.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import json
+from os.path import expanduser
+from pathlib import Path
+
+import requests
+from bs4 import BeautifulSoup
+from html2text import html2text
+
+# from datetime import datetime
+
+
+RESULTS_FILE = Path(expanduser('~/lbc_results.json'))
+QUERY_FILE = Path(expanduser('~/lbc_queries.json'))
+
+if __name__ == '__main__':
+    with QUERY_FILE.open() as f:
+        urls = json.load(f)
+
+    if RESULTS_FILE.is_file():
+        with RESULTS_FILE.open() as f:
+            results = json.load(f)
+    else:
+        print('first email')
+        results = {key: {} for key in urls.keys()}
+
+    for key, url in urls.items():
+        soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+        for link in soup.find_all('a', class_='list_item'):
+            href = link.attrs['href']
+            if href.startswith('//'):
+                href = 'https:' + href
+            if href not in results[key]:
+                print(f'Nouveau résultat pour {key}: {href}')
+                results[key][href] = {}
+        for url, data in results[key].items():
+            sousoup = BeautifulSoup(requests.get(url).content, 'html.parser')
+            results[key][url] = {
+                'titre': sousoup.find('h1').text,
+                'prix': sousoup.find('span', class_='_1F5u3').text,
+                # 'date': datetime.strptime(sousoup.find('div', class_='_3Pad-').text, '%d/%m/%Y à %Hh%M'),
+                'date': sousoup.find('div', class_='_3Pad-').text,
+                'critères': [s.text for s in sousoup.find_all(class_='_3Jxf3')],
+                'description': [l.strip() for l in html2text(str(sousoup.find(class_='_2wB1z'))).split('\n') if l],
+            }
+
+    with RESULTS_FILE.open('w') as f:
+        json.dump(results, f)

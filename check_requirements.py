966a496ee3490e58b8f7825443616b98434578a5
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-10-27 17:37:06 +0200

    stop looking at requirements.txt

--- check_requirements.py
+++ check_requirements.py
@@ -17,7 +17,6 @@ TOKEN = check_output(['pass', TOKEN_PASS_PATH]).decode().split()[0]
 HEADERS = {'Authorization': f'token {TOKEN}'}
 
 PyPI = 'https://pypi.python.org/pypi/%s/json'
-REQUIREMENTS = 'https://raw.githubusercontent.com/%s/master/requirements.txt'
 PIPFILE_LOCK = 'https://raw.githubusercontent.com/%s/master/Pipfile.lock'
 REPOS = 'https://api.github.com/user/repos'
 
@@ -60,20 +59,7 @@ def get_all():
     all_packages = set()
     all_repos = sys.argv[1:] if len(sys.argv) > 1 else get_all_repos()
 
-    # Parse the requirements.txt file from each repo from {all_repos}
-    for r in dl(REQUIREMENTS, all_repos, 'requirements.txt'):
-        if r.status_code == 200:
-            full_name = '/'.join(r.url.split('/')[3:5])
-            try:
-                packages = [line.split()[0].split('==') for line in r.content.decode().split('\n')
-                            if line and not line.startswith('#') and '#egg=' not in line]
-                repos[full_name] = {package: version for package, version in packages}
-                for package in packages:
-                    all_packages.add(package[0].split('[')[0])
-            except Exception as e:
-                print(f'Error on {full_name}: {e}')
-
-    # Parse the Pipfile.lock file from each repo from {all_repos}, overrides older data from requirements.txt
+    # Parse the Pipfile.lock file from each repo from {all_repos}
     for r in dl(PIPFILE_LOCK, all_repos, 'Pipfile.lock'):
         if r.status_code == 200:
             full_name = '/'.join(r.url.split('/')[3:5])

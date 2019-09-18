870a9d660e209516625ef8796466a7c53448bc47
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-09-18 16:24:04 +0200

    nim_docker: fish for stretch / buster

--- nim_docker.sh
+++ nim_docker.sh
@@ -4,7 +4,7 @@
 
 set -ex
 
-FD_VERSION=7.3.0
+FD_VERSION=7.4.0
 RG_VERSION=11.0.2
 FISH_VERSION=3.0.2
 
@@ -23,10 +23,19 @@ if [[ -f /etc/debian_version ]]
 then
     FD="https://github.com/sharkdp/fd/releases/download/v${FD_VERSION}/fd_${FD_VERSION}_amd64.deb"
     RG="https://github.com/BurntSushi/ripgrep/releases/download/${RG_VERSION}/ripgrep_${RG_VERSION}_amd64.deb"
-    FISH="https://launchpad.net/~fish-shell/+archive/ubuntu/release-3/+files/fish_${FISH_VERSION}-1~$(lsb_release -cs)_amd64.deb"
-    FISH_COMMON="https://launchpad.net/~fish-shell/+archive/ubuntu/release-3/+files/fish-common_${FISH_VERSION}-1~$(lsb_release -cs)_all.deb"
+    wget "$FD" "$RG"
 
-    wget "$FD" "$RG" "$FISH" "$FISH_COMMON"
+    DEBIAN_VERSION=$(lsb_release-cs)
+    if [[ ${DEBIAN_VERSION} == buster ]]
+    then
+        apt install -qqy fish
+    elif [[ ${DEBIAN_VERSION} != stretch ]]
+    then
+        FISH="https://launchpad.net/~fish-shell/+archive/ubuntu/release-3/+files/fish_${FISH_VERSION}-1~${DEBIAN_VERSION}_amd64.deb"
+        FISH_COMMON="https://launchpad.net/~fish-shell/+archive/ubuntu/release-3/+files/fish-common_${FISH_VERSION}-1~${DEBIAN_VERSION}_all.deb"
+        wget "$FISH" "$FISH_COMMON"
+    fi
+
     dpkg -i ./*.deb
     rm ./*.deb
 fi
@@ -40,7 +49,7 @@ cd
 for file in .bash_profile .bash_logout .tmux.conf .nanorc .vimpagerrc .vimrc .Xdefaults .gitconfig .bashrc .hgrc .zshrc .xmonad .vim .xinitrc .compton.conf .editorconfig .ipython .imapfilter .notmuch-config .msmtprc .pypirc
 do
     [[ -L $file || -f $file ]] && rm $file
-    ln -s $HOME/dotfiles/$file
+    ln -s "$HOME/dotfiles/$file" .
 done
 
 mkdir -p .config
@@ -48,5 +57,5 @@ cd .config
 for files in awesome dfc fish pep8 ranger terminator zathura flake8 terminology fontconfig khal khard vdirsyncer todoman offlineimap mutt i3 i3status rofi dunst pylintrc
 do
     [[ -L $files ]] && rm $files
-    ln -s $HOME/dotfiles/.config/$files
+    ln -s "$HOME/dotfiles/.config/$files" .
 done

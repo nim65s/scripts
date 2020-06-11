8a190f486092066c1dec6a9351d12d2c8719387b
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-06-11 16:25:49 +0200

    latexmk

--- install.sh
+++ install.sh
@@ -68,7 +68,7 @@ do
     popd
 done
 
-for file in .bash_profile .bash_logout .tmux.conf .nanorc .vimpagerrc .vimrc .Xdefaults .gitconfig .bashrc .hgrc .zshrc .xmonad .vim .xinitrc .compton.conf .editorconfig .ipython .imapfilter .notmuch-config .msmtprc .pypirc
+for file in .bash_profile .bash_logout .tmux.conf .nanorc .vimpagerrc .vimrc .Xdefaults .gitconfig .bashrc .hgrc .zshrc .xmonad .vim .xinitrc .compton.conf .editorconfig .ipython .imapfilter .notmuch-config .msmtprc .pypirc .latexmk
 do
     [[ -L $file || -f $file ]] && rm $file
     ln -s $HOME/dotfiles/$file

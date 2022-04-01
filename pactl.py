4c7e5428a5df66616ecbfe257c6ec30012c3a35f
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-04-02 00:17:12 +0200

    pactl: move parse in separate methods

--- pactl.py
+++ pactl.py
@@ -17,6 +17,8 @@ SET_VOL = "Set volume in 0-65536 / %% / dB"
 class PactlWrapper:
     def __init__(self):
         self.args = self.get_parser().parse_args()
+        self.sink_inputs = {}
+        self.sources = {}
 
     def get_parser(self):
         parser = ArgumentParser(description=__doc__)
@@ -41,44 +43,60 @@ class PactlWrapper:
 
     def main(self, **kwargs):
         """TODO."""
+        self.get_sink_inputs()
+        self.get_sources()
         print("not yet implemented")
 
     def lines(self, cmd: [str]):
         """Helper to get text lines out of a command."""
         return check_output(cmd, text=True).split("\n")
 
-    def application(self, name: str, set_vol: Optional[str] = None, **kwargs):
-        """Applications are "sink inputs"."""
+    def get_sink_inputs(self, name: Optional[str] = None) -> Optional[str]:
+        """Parse list of sink-inputs, optionnaly looking for a particular name."""
         for line in self.lines(["pactl", "list", "sink-inputs"]):
             if line.startswith(SINK_INPUT):
                 sink = int(line.removeprefix(SINK_INPUT))
             line = line.strip()
             if line.startswith(GET_VOL):
                 get_vol = line.removeprefix(GET_VOL)
-            if line.startswith(APP_NAME) and name in line:
-                name = line.removeprefix(APP_NAME)
-                break
-        else:
+            if line.startswith(APP_NAME):
+                app_name = line.removeprefix(APP_NAME).strip('"')
+                self.sink_inputs[app_name] = (sink, get_vol)
+                if name and name in app_name:
+                    return app_name
+
+    def application(self, name: str, set_vol: Optional[str] = None, **kwargs):
+        """Applications are "sink inputs"."""
+        app_name = self.get_sink_inputs(name)
+        if not app_name:
             raise ValueError(f"'{name}' not found in sink-inputs")
+        sink, get_vol = self.sink_inputs[app_name]
         if set_vol:
             run(["pactl", "set-sink-input-volume", str(sink), set_vol])
             print(f"application {name} volume set to: {set_vol}")
         else:
             print(f"application {name} has volume: {get_vol}")
 
-    def microphone(self, name: str, set_vol: Optional[str] = None, **kwargs):
-        """Microphones are "sources"."""
+    def get_sources(self, name: Optional[str] = None) -> Optional[str]:
+        """Parse list of sources, optionnaly looking for a particular name."""
         for line in self.lines(["pactl", "list", "sources"]):
             if line.startswith(SOURCE):
                 source = int(line.removeprefix(SOURCE))
             line = line.strip()
             if line.startswith(GET_VOL):
                 get_vol = line.removeprefix(GET_VOL)
-            if line.startswith(ALSA_NAME) and name in line:
-                name = line.removeprefix(ALSA_NAME)
-                break
-        else:
+            if line.startswith(ALSA_NAME):
+                alsa_name = line.removeprefix(ALSA_NAME).strip('"')
+                self.sources[alsa_name] = (source, get_vol)
+                if name and name in alsa_name:
+                    return alsa_name
+
+    def microphone(self, name: str, set_vol: Optional[str] = None, **kwargs):
+        """Microphones are "sources"."""
+        alsa_name = self.get_sources(name)
+        if not alsa_name:
             raise ValueError(f"'{name}' not found in sources")
+        source, get_vol = self.sources[alsa_name]
         if set_vol:
             run(["pactl", "set-source-volume", str(source), set_vol])
             print(f"microphone {name} volume set to: {set_vol}")

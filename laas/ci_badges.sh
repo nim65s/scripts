6398e610b707229f0b29e6181413a00500448189
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-04-04 13:54:57 +0200

    update tooling

--- laas/ci_badges.sh
+++ laas/ci_badges.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/bin/bash -eux
 
-ORG=${1}
-PRJ=${2}
+ORG=${1%/*}
+PRJ=${1#*/}
 
 GL=gitlab.laas.fr
 GH=github.com
 
 ML=gsaurel
 MH=nim65s
-set -e
+
+browse https://rainboard.laas.fr/project/$PRJ/robotpkg
 
 for url in $GL/$ML $GH/$MH $GL/$ORG $GH/$ORG
 do
@@ -23,24 +24,53 @@ cd $ORG/$PRJ
 
 git checkout devel
 git pull --rebase main devel
+git pull --rebase github devel
 git submodule update
 
-hostname -f | grep -q laas.fr || export ALL_PROXY="socks5h://localhost:1445"
+#hostname -f | grep -q laas.fr || export ALL_PROXY="socks5h://localhost:1445"
 echo "include: http://rainboard.laas.fr/project/$PRJ/.gitlab-ci.yml" > .gitlab-ci.yml
+
+TEMPLATE=$HOME/local/template
+if fd -q -E cmake -e py
+then
+    [[ -f setup.cfg ]] && $EDITOR setup.cfg || cp $TEMPLATE/setup.cfg .
+    [[ -f pyproject.toml ]] && $EDITOR pyproject.toml || cp $TEMPLATE/pyproject.toml .
+fi
+[[ -f .pre-commit-config.yaml ]] && $EDITOR .pre-commit-config.yaml || cp $TEMPLATE/.pre-commit-config.yaml .pre-commit-config.yaml
+[[ -f .clang-format ]] && rm .clang-format
 
 cat >> README.md << EOF
 
 [![Pipeline status](https://gitlab.laas.fr/$ORG/$PRJ/badges/master/pipeline.svg)](https://gitlab.laas.fr/$ORG/$PRJ/commits/master)
 [![Coverage report](https://gitlab.laas.fr/$ORG/$PRJ/badges/master/coverage.svg?job=doc-coverage)](http://projects.laas.fr/gepetto/doc/$ORG/$PRJ/master/coverage/)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![pre-commit.ci status](https://results.pre-commit.ci/badge/github/$ORG/$PRJ/master.svg)](https://results.pre-commit.ci/latest/github/$ORG/$PRJ)
 
 EOF
 
 $EDITOR README.md
 git add .
-git commit
+git commit -m 'update tools & badges' || true
 git submodule foreach git checkout master
 git submodule foreach git pull
-git commit -am 'sync submodules'
-git push origin devel
+git commit -am 'sync submodules' || true
 
-echo ./mirror.sh $ORG $PRJ
+pre-commit install
+pre-commit run -a || true
+fd -q -E cmake -e py && black . || true
+while ! flake8 .
+do sleep 1
+done
+
+git commit -am 'format' || true
+
+git log --oneline --grep 'lint\|yapf\|format' "--format=format:# %s (%an, %as)%n%H%n" >> .git-blame-ignore-revs
+$EDITOR .git-blame-ignore-revs
+
+git add .git-blame-ignore-revs
+git commit -m 'git blame ignore revs' || true
+
+git push origin devel
+browse https://$GL/$ML/$PRJ/pipelines
+
+#echo ./mirror.sh $ORG $PRJ

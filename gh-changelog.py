005285ade3a667b9966cdf2eaa3dea1427918903
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-11-09 16:21:41 +0100

    gh changelog: format details

--- gh-changelog.py
+++ gh-changelog.py
@@ -37,9 +37,16 @@ def main(token, owner, repo, page):
             # fixed in python < 3.11 can't deal with Z
             published_at = release["published_at"].removesuffix("Z")
             date = datetime.fromisoformat(published_at).strftime("%Y-%m-%d")
+            body = release["body"].replace("\r", "")
+            body = body.replace("\n# ", "\n### ")
+            body = body.replace("\n## ", "\n### ")
+            if body.startswith("# "):
+                body = f"##{body}"
+            elif body.startswith("## "):
+                body = f"#{body}"
             print(f"## [{tag}] - {date}")
             print()
-            print(release["body"].replace("\n#", "\n##"))
+            print(body)
             print()
         page += 1
 

f05c21e7cc33b6e3ffca8ccab35549b21f357cff
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-11-21 17:20:18 +0100

    gorg: rbw get github-token

--- gorg.py
+++ gorg.py
@@ -107,7 +107,14 @@ if __name__ == "__main__":
         level = 30 - 10 * args.verbose
     logging.basicConfig(level=level)
     main(
-        args.token or os.environ["GITHUB_TOKEN"],
+        args.token
+        or os.environ.get(
+            "GITHUB_TOKEN",
+            subprocess.check_output(
+                os.environ.get("GITHUB_TOKEN_CMD", "rbw get github-token").split(),
+                text=True,
+            ).strip(),
+        ),
         # args.gl_token or os.environ["GITLAB_TOKEN"],
         args.orgs,
         args.page,

36f395a21b22c352b6d1bcde996a881685cda74c
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-01-28 11:58:59 +0100

    md5cron: default to current dir

--- md5cron.py
+++ md5cron.py
@@ -6,9 +6,10 @@ and print it in a cron format
 
 from argparse import ArgumentParser
 from hashlib import md5
+from pathlib import Path
 
 parser = ArgumentParser(description=__doc__)
-parser.add_argument("project")
+parser.add_argument("project", default=Path.cwd().name, nargs="?")
 
 
 def md5cron(project: str) -> str:
@@ -21,6 +22,7 @@ def md5cron(project: str) -> str:
 
     n = int(md5(project.encode()).hexdigest(), 16)
     hour, day = (n // 30) % 24, n % 30 + 1
+    print(f"# {project=}")
     print(f"0 {hour} {day} * *")
 
 
